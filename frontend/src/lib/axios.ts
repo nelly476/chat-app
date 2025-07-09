2e751c07c23216ad3c1d5b7b6f49eb93687b5b89
Nelly <nellybulatova12@email.com>
2025-07-09 11:07:44 +0300

    fix axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : '/api',
-  baseURL: 'http://localhost:5001/api',
+  baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : '/api',
+  // baseURL: 'http://localhost:5001/api',
   withCredentials: true,
 
 });
