5e03974a9b7f945fce6fc42ebbf9a7441617126c
Nelly <nellybulatova12@email.com>
2025-07-26 11:54:39 +0300

    attempt 1

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -32,6 +32,7 @@ export const LoginPage:React.FC<LoginPageProps> = () => {
 //      "email": "ndoe@gmail.com",
 //      "password": "1113333"
 // }
+// rashid@mail.com 111111
 
     return (
         <div className="h-screen grid lg:grid-cols-2">
