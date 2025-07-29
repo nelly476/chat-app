b5608ce8404e802dae390d857b895acf1a1a4455
Nelly <nellybulatova12@email.com>
2025-07-29 11:07:16 +0300

    add docker

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -37,6 +37,7 @@ export const LoginPage:React.FC<LoginPageProps> = () => {
 // rashid@mail.com 111111
 
 // add some changes
+// alice@mail.com 222222
 
     return (
         <div className="h-screen grid lg:grid-cols-2">
